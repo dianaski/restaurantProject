cfe42e29373a4c94a89f1e9283e8dddfb7b6506e
Diana <dianaskitsko1989@gmail.com>
2023-11-20 13:47:52 +0100

    added navber file

---
+++ view/navbar.html.php
@@ -0,0 +1,38 @@
+<!-- NAVBAR -->
+ 
+<div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <a class="navbar-brand" href="index.php">
+                Restaurant Project
+                </a>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php">Home</a>
+                        </li>
+
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="listaPiatti.php">Menu</a>
+                        </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="login.php"><i class="bi bi-person"></i></a></button>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="register.php"><i class="bi bi-box-arrow-in-right"></i></a>
+                        </li>
+                    
+                       
+                    </ul>
+               
+                    </div>
+                
+
+            </div>
+   
+<!-- NAV END -->
